25fb99faf5fa15fec336838f5b91791c230f9bab
Ram <raamkinkar.rout533@gmail.com>
2022-01-20 20:40:42 +0530

    Added Board X and O state logic

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 const Square = props => {
-  const { value } = props;
+  const { value, onClick } = props;
   //   console.log(value);
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
